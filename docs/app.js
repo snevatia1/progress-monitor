79a54aadf4a72f2ef72f3e2b1d6ed60b068f296e
snevatia1 <snevatia1@gmail.com>
2025-12-23 20:29:23 +0530

    Refactor photoId generation to use makeId function

--- docs/app.js
+++ docs/app.js
@@ -1,2 +1,6 @@
-const photoId = crypto.randomUUID();
+function makeId() {
+  return (crypto && crypto.randomUUID) ? crypto.randomUUID() :
+    ("id_" + Date.now() + "_" + Math.random().toString(16).slice(2));
+}const photoId = makeId();
+
 

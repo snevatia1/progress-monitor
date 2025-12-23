e1db9073e9d1409d9d4609b28990d67caaf052d2
snevatia1 <snevatia1@gmail.com>
2025-12-23 19:57:17 +0530

    Add initial configuration for Progress Monitor app

---
+++ docs/config.js
@@ -0,0 +1,25 @@
+window.APP_CONFIG = {
+  APP_NAME: "Progress Monitor",
+  TIMEZONE: "Asia/Kolkata",
+
+  DEFAULT_WINDOWS: {
+    MORNING: "09:00",
+    BEFORE_LUNCH: "13:00",
+    EVENING: "17:30"
+  },
+
+  UPLOAD_ENDPOINT: "https://script.google.com/macros/s/AKfycbyV4kqQT5vWB5Fck5U1e0fOWbaOEbLP28MVskePktQ1rZhQDTbel4ont-8lK_jv7TGv/exec",
+
+  WORKER_ID: "worker_001",
+  WORKER_NAME_HI: "वर्कर 001",
+
+  SITES: [
+    {
+      siteId: "SITE_1",
+      nameHi: "साइट 1",
+      expectedLat: null,     // add later
+      expectedLng: null,
+      geoRadiusM: null
+    }
+  ]
+};
